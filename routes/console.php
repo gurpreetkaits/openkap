0ff89488818fbaf9126be1cb7b5dabe026a46e78
Gurpreet Kait <gurpreetkait.codes@gmail.com>
2026-01-17 23:35:58 +0530

    fix

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
 
 // Process queue jobs every minute
 Schedule::command('queue:work --stop-when-empty --max-time=50')
